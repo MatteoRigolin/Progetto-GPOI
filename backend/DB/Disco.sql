fdee0f782bc37c20792dab40a1d24d90f7398506
andreamarchetto04 <72484739+andreamarchetto04@users.noreply.github.com>
2023-05-03 15:48:38 +0200

    active
    aggiunta campi active

--- backend/DB/Disco.sql
+++ backend/DB/Disco.sql
@@ -8,13 +8,15 @@ date_event			date not null,
 start_hour			time not null,
 end_hour 			time not null,
 description			nvarchar(500) not null,
-id_room				int unsigned not null
+id_room				int unsigned not null,
+event_active		BOOLEAN  NOT NULL DEFAULT (TRUE)
 );
 
 create table room(
 id 					INT UNSIGNED NOT NULL   AUTO_INCREMENT  PRIMARY KEY,
 capacity 			int unsigned not null,
-room_description			nvarchar(500) not null
+room_description	nvarchar(500) not null,
+room_active			BOOLEAN  NOT NULL DEFAULT (TRUE)
 );
 
 ALTER TABLE event ADD CONSTRAINT fk_event_room FOREIGN KEY ( id_room ) REFERENCES room ( id );
