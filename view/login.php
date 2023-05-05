b0b0a395ff74f3d9a33870d9fb74057b813f8e50
enricopaolotoso <74261261+enricopaolotoso@users.noreply.github.com>
2023-05-06 00:23:59 +0200

    Login Frontend finished

--- view/login.php
+++ view/login.php
@@ -1,18 +1,24 @@
-<!doctype html>
-<html lang="en">
+<head>
+    <link rel="stylesheet" href="login.css">
+    <link rel="stylesheet" href="file.scss">
+</head>
+<div class="container" id="container">
+<div class="form-container sign-up-container">
+        <form action="#">
+            <h1>Crea Account</h1>
+            <input type="text" placeholder="Name" />
+            <input type="email" placeholder="Email" />
+            <input type="password" placeholder="Password" />
+            <button>Registrati</button>
+        </form>
+    </div>
+    <div class="form-container sign-in-container">
+        <form action="#">
+            <h1>Accedi</h1>
+            <input type="email" placeholder="Email" />
+            <input type="password" placeholder="Password" />
 
-<body>
-    <form class="form-signin" method="post">
-       
-        <h1 class="h3 mb-3 fw-bold">Inserisci le credenziali</h1>
-        <label for="inputEmail" class="sr-only mb-2">Email o Username</label>
-        <input type="text" id="inputEmail" class="form-control mb-4" placeholder="Email o Username" name="email" required
-            autofocus>
-        <label for="inputPassword" class="sr-only mb-2">Password</label>
-        <input type="password" id="inputPassword" class="form-control mb-4" placeholder="Password" name="pw"
-            required>
-
-        <?php
+            <?php
         session_start();
 
         include_once dirname(__FILE__) . '\function\user.php';
@@ -43,92 +49,24 @@
         }
         ?>
 
-        <div class="row">
-            <button class="btn btn-lg btn-primary btn-block mx-auto" type="submit">Accedi</button>
-            <div class="row">
-                <a class="text-dark" href="../view/index.php?page=7" style="text-decoration: none; font-size:13px;">
-                    <u>Se non hai ancora un account, registrati ora!</u>
-                </a>
+            <a href="#">Password dimenticata?</a>
+            <button>Accedi</button>
+        </form>
+    </div>
+    <div class="overlay-container">
+        <div class="overlay">
+            <div class="overlay-panel overlay-left">
+                <h1>Bentornato!</h1>
+                <p>Per accedere inserisci i tuoi dati</p>
+                <button class="ghost" id="signIn">Sign In</button>
+            </div>
+            <div class="overlay-panel overlay-right">
+                <h1>Ciao, Boss!</h1>
+                <p>Inserisci i tuoi dati e inizia!</p>
+                <button class="ghost" id="signUp">Registrati</button>
             </div>
         </div>
-    </form>
+    </div>
+</div>
 
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js"
-        integrity="sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN"
-        crossorigin="anonymous"></script>
-</body>
-
-</html>
-
-<!--<style>
-    
-
-    body {
-        display: -ms-flexbox;
-        display: -webkit-box;
-        display: flex;
-        -ms-flex-align: center;
-        -ms-flex-pack: center;
-        -webkit-box-align: center;
-        align-items: center;
-        -webkit-box-pack: center;
-        justify-content: center;
-        padding-top: 40px;
-        padding-bottom: 40px;
-        background-color: #f5f5f5;
-    }
-
-    .form-signin {
-        width: 100%;
-        max-width: 330px;
-        padding: 15px;
-        margin: 0 auto;
-    }
-
-    .form-signin .checkbox {
-        font-weight: 400;
-    }
-
-    .form-signin .form-control {
-        position: relative;
-        box-sizing: border-box;
-        height: auto;
-        padding: 10px;
-        font-size: 16px;
-    }
-
-    .form-signin .form-control:focus {
-        z-index: 2;
-    }
-
-    .form-signin input[type="email"] {
-        margin-bottom: -1px;
-        /* border-bottom-right-radius: 0; */
-        /* border-bottom-left-radius: 0; */
-    }
-
-    .form-signin input[type="password"] {
-        margin-bottom: 10px;
-        /* border-top-left-radius: 0; */
-        /* border-top-right-radius: 0; */
-    }
-
-    body, html {
-  height: 100%;
-}
-
-.bg {
-  /* The image used */
-  background-image: url("../assets/img/campocalcio.jpg");
-
-  /* Full height */
-  height: 95%;
-
-  /* Center and scale the image nicely */
-  background-position: center;
-  background-repeat: no-repeat;
-  background-size: cover;
-}
-</style>-->
-
-</html>
+<script src="login.js"></script>
