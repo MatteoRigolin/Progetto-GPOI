53fef31f1ad09244b95143258065da8407077b08
andreamarchetto04 <72484739+andreamarchetto04@users.noreply.github.com>
2023-05-05 11:03:27 +0200

    function
    login e registration

---
+++ view/function/user.php
@@ -0,0 +1,76 @@
+<?php
+
+function login($data)
+{
+    $url = 'http://localhost/Progetto-GPOI/backend/controller/user/login.php';
+
+    $curl = curl_init($url); //inizializza una nuova sessione di cUrl
+    //Curl contiene il return del curl_init 
+
+    curl_setopt($curl, CURLOPT_URL, $url); // setta l'url 
+    curl_setopt($curl, CURLOPT_POST, true); // specifica che è una post request
+    curl_setopt($curl, CURLOPT_RETURNTRANSFER, true); // ritorna il risultato come stringa
+
+
+    $headers = array(
+        "Content-Type: application/json",
+        "Content-Lenght: 0",
+    );
+
+
+    curl_setopt($curl, CURLOPT_HTTPHEADER, $headers); // setta gli headers della request
+
+    curl_setopt($curl, CURLOPT_POSTFIELDS, json_encode($data));
+
+    $responseJson = curl_exec($curl); //eseguo
+
+    curl_close($curl); //chiudo sessione
+
+    $response = json_decode($responseJson); //decodifico la response dal json
+
+    if ($response == true) //response == true vuol dire sessione senza errori
+    {
+        $_SESSION['user_id'] = $response->userID;
+
+    } else {
+        return -1;
+    }
+}
+
+function logon($data)
+{
+    $url = 'http://localhost/Progetto-GPOI/backend/controller/user/registration.php';
+
+    $curl = curl_init($url); //inizializza una nuova sessione di cUrl
+    //Curl contiene il return del curl_init 
+
+    curl_setopt($curl, CURLOPT_URL, $url); // setta l'url 
+    curl_setopt($curl, CURLOPT_POST, true); // specifica che è una post request
+    curl_setopt($curl, CURLOPT_RETURNTRANSFER, true); // ritorna il risultato come stringa
+
+
+    $headers = array(
+        "Content-Type: application/json",
+        "Content-Lenght: 0",
+    );
+
+
+    curl_setopt($curl, CURLOPT_HTTPHEADER, $headers); // setta gli headers della request
+
+    curl_setopt($curl, CURLOPT_POSTFIELDS, json_encode($data));
+
+    $responseJson = curl_exec($curl); //eseguo
+
+    curl_close($curl); //chiudo sessione
+
+    $response = json_decode($responseJson); //decodifico la response dal json
+
+    if ($response-> message == "1") //response == true vuol dire sessione senza errori
+    {
+        header('Location: ../login.php');
+    } else {
+        return -1;
+    }
+}
+
+?>
